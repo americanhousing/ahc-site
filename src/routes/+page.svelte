35bb2221c516cbca542008307c8bcb75229b4a60
andrey yazev <yazev@me.com>
2025-12-04 19:51:28 +0100

    update website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,9 @@
 		src="https://embed.lu.ma/checkout-button.js"></script>
 </svelte:head>
 
-<div class="box-border flex h-screen gap-6 p-4 md:p-6">
-	<div class="flex min-w-80 flex-1/3 flex-col">
+<div
+	class="mx-auto box-border flex min-h-screen max-w-[1440px] gap-6 px-4 md:px-6">
+	<div class="flex min-w-96 flex-1/3 flex-col pt-6">
 		<img
 			src="/images/logo-color.svg"
 			width="101"
@@ -14,10 +15,10 @@
 		<div class="h-6 md:h-8"></div>
 
 		<h1 class="font-die-d text-[48px] font-medium md:text-[64px]">
-			House Party
+			Open House
 		</h1>
 
-		<div class="hidden flex-1 md:block"></div>
+		<div class="hidden min-h-2 flex-1 md:block"></div>
 		<div
 			class="bg-driveway/30 mb-4 aspect-[360/285] h-auto w-full rounded-[2px] md:hidden">
 		</div>
@@ -30,11 +31,11 @@
 			<div class="text-driveway font-medium">
 				The American Housing Corporation is excited to invite you to an
 				exclusive preview of our first house prototype and the upcoming
-				product line up in our Austin, TX factory.
+				product lineup in our Austin, TX factory.
 			</div>
 		</div>
 
-		<div class="bg-driveway my-2 h-[0.5px] md:my-4"></div>
+		<div class="bg-driveway my-2 min-h-[0.5px] md:my-4"></div>
 		<div
 			class="flex flex-col gap-2 text-[18px] leading-[24px] md:gap-4 md:text-[24px]">
 			<div class="text-driveway/60 font-medium">Date & Time</div>
@@ -44,27 +45,33 @@
 			</div>
 		</div>
 
-		<div class="bg-driveway my-2 h-[0.5px] md:my-4"></div>
+		<div class="bg-driveway my-2 min-h-[0.5px] md:my-4"></div>
 		<div
 			class="flex flex-col gap-2 text-[18px] leading-[24px] md:gap-4 md:text-[24px]">
 			<div class="text-driveway/60 font-medium">Location</div>
 			<div class="text-driveway font-medium">Register to see address</div>
 		</div>
 
-		<div class="bg-driveway my-2 h-[0.5px] md:my-4"></div>
-		<div class="h-2 md:hidden"></div>
-		<a
-			class="font-die-a bg-blue hover:bg-blue/60 flex cursor-pointer justify-center gap-2 rounded-full px-4 py-3 text-[18px] leading-4 text-white md:self-start"
-			href="https://luma.com/event/evt-Kqb67T3Xqzx4OIh"
-			data-luma-action="checkout"
-			data-luma-event-id="evt-Kqb67T3Xqzx4OIh">
-			RSVP
-			<img
-				src="/icons/arrow-right.svg"
-				width="14"
-				height="8"
-				alt="RSVP" />
-		</a>
+		<div class="bg-driveway my-2 min-h-[0.5px] md:my-4"></div>
+		<div class="min-h-18 md:min-h-16"></div>
+		<div class="sticky bottom-6 h-0">
+			<a
+				class="font-die-a bg-blue hover:bg-blue/60 absolute bottom-full flex cursor-pointer justify-center gap-2 rounded-full px-4 py-3 text-[18px] leading-4 text-white md:self-start"
+				href="https://luma.com/event/evt-Kqb67T3Xqzx4OIh"
+				data-luma-action="checkout"
+				data-luma-event-id="evt-Kqb67T3Xqzx4OIh">
+				RSVP
+				<img
+					src="/icons/arrow-right.svg"
+					width="14"
+					height="8"
+					alt="RSVP" />
+			</a>
+		</div>
 	</div>
-	<div class="bg-driveway/30 hidden flex-2/3 rounded-[2px] md:block"></div>
+	<div class="hidden flex-2/3 md:block">
+		<div class="sticky top-0 box-border h-screen py-6">
+			<div class="bg-driveway/30 h-full rounded-[2px]"></div>
+		</div>
+	</div>
 </div>
