7e40c45392a68c625ba1c8254af2fdeed387b827
andrey yazev <yazev@me.com>
2025-12-15 13:15:35 +0100

    add mobile styles to mission page and how we work section with new type utilities

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -10,44 +10,49 @@
 		<div
 			class="mx-auto min-h-[40vh] max-w-[1440px] flex-col px-4 md:flex-row md:px-6">
 			<div
-				class="bg-driveway/20 h-auto w-full rounded-[2px] md:aspect-[1392/690]">
+				class="bg-driveway/20 aspect-[360/390] h-auto w-full rounded-[2px] md:aspect-[1392/690]">
 			</div>
-			<div class="h-[110px] md:h-[84px]"></div>
+			<div class="h-[24px] md:h-[84px]"></div>
 			<img
 				class="w-full"
 				src="/images/united-we-build.svg"
 				width="1328"
 				height="160"
 				alt="United We Build" />
-			<div class="h-[110px] md:h-[84px]"></div>
+			<div class="h-[24px] md:h-[84px]"></div>
 			<p
-				class="type-display text-cumulus font-gt-super-book text-balance">
+				class="type-body md:type-display text-cumulus font-gt-super-book md:text-balance">
 				We won independence, expanded freedom, beat a depression at home
 				and spread democracy abroad. We united the continent with
 				highways, raised skyscrapers, put men on the moon.
 			</p>
-			<div class="h-[110px] md:h-[84px]"></div>
-			<div class="flex">
+			<div class="h-[48px] md:h-[84px]"></div>
+			<div class="flex flex-col md:flex-row">
 				<div
 					class="bg-driveway/20 mr-6 aspect-[560/644] h-auto w-full flex-1/2 rounded-[2px] lg:aspect-[684/855]">
 				</div>
+				<div class="h-[24px] md:hidden"></div>
 				<div
 					class="md:border-l-cumulus flex flex-1/2 flex-col md:border-l-1 md:pl-6">
 					<div
-						class="text-cumulus type-display font-medium text-balance">
+						class="text-cumulus type-body md:type-display font-medium text-balance">
 						Over 250 years, a simple truth took form: the American
 						Dream is built, not inherited.
 					</div>
 				</div>
 			</div>
-			<div class="h-[110px] md:h-[84px]"></div>
-			<p class="type-display text-cumulus font-gt-super-book">
+			<div class="h-[48px] md:h-[84px]"></div>
+			<p
+				class="type-body md:type-display text-cumulus font-gt-super-book">
 				Now it’s our turn to build something worthy of the name
 				“American.”
 			</p>
-			<p class="type-display text-cumulus font-gt-super-book"><br /></p>
 			<p
-				class="type-display text-cumulus font-gt-super-book text-balance">
+				class="type-body md:type-display text-cumulus font-gt-super-book">
+				<br />
+			</p>
+			<p
+				class="type-body md:type-display text-cumulus font-gt-super-book text-balance">
 				Today, many who say the American Dream is dead are really
 				talking about housing. Too many families can’t afford a starter
 				home in the cities they love. From the moment they plan for a
@@ -56,56 +61,97 @@
 				that impossible, forcing people to choose between putting down
 				roots or starting a family elsewhere.
 			</p>
-			<div class="h-[110px] md:h-[84px]"></div>
-			<div class="flex">
+			<div class="h-[48px] md:h-[84px]"></div>
+			<div class="flex flex-col md:flex-row">
 				<div
 					class="bg-driveway/20 mr-6 aspect-[560/644] h-auto w-full flex-1/2 rounded-[2px] lg:aspect-[684/855]">
 				</div>
+				<div class="h-[24px] md:hidden"></div>
 				<div
 					class="md:border-l-cumulus flex flex-1/2 flex-col md:border-l-1 md:pl-6">
 					<div
-						class="text-cumulus type-display font-medium text-balance">
+						class="text-cumulus type-body md:type-display font-medium text-balance">
 						<p>
 							When families are priced out, cities hollow out. And
 							as cities go, so goes America.
 						</p>
 						<p><br /></p>
-						<p>That’s why we build.</p>
+						<p>That's why we build.</p>
 					</div>
 				</div>
 			</div>
-			<div class="h-[110px] md:h-[84px]"></div>
-			<p class="type-display text-cumulus font-gt-super-book">
-				We’re an organization of engineers, designers, architects, and
+			<div class="h-[48px] md:h-[84px]"></div>
+			<p
+				class="type-body md:type-display text-cumulus font-gt-super-book">
+				We're an organization of engineers, designers, architects, and
 				operators who believe housing is the cornerstone of the American
 				Dream. Some of us come from construction sites, some from rocket
 				factories, some from software labs. But we’re united by a shared
 				instinct: make it real.
 			</p>
-			<p class="type-display text-cumulus font-gt-super-book"><br /></p>
-			<p class="type-display text-cumulus font-gt-super-book">
-				Together, we’re applying advanced engineering to increase
+			<p
+				class="type-body md:type-display text-cumulus font-gt-super-book">
+				<br />
+			</p>
+			<p
+				class="type-body md:type-display text-cumulus font-gt-super-book">
+				Together, we're applying advanced engineering to increase
 				quality and lower cost for the most important product on earth.
 				We’re not the first to make this promise — but we’re the first
 				to unite technology, manufacturing, and real estate under one
 				roof, fully integrated from end to end.
 			</p>
-			<p class="type-display text-cumulus font-gt-super-book"><br /></p>
-			<p class="type-display text-cumulus font-gt-super-book">
+			<p
+				class="type-body md:type-display text-cumulus font-gt-super-book">
+				<br />
+			</p>
+			<p
+				class="type-body md:type-display text-cumulus font-gt-super-book">
 				Total integration gives us total control over what we build, how
 				we build, and where we build it. In doing so, we’re not just
 				building homes — we’re building a new housing industry: precise,
 				scalable, automated, and powered by American ingenuity.
 			</p>
-			<div class="h-[110px] md:h-[84px]"></div>
-			<div class="flex">
+			<div class="h-[48px] md:h-[120px]"></div>
+			<section>
+				<div
+					class="type-subhead border-b-cumulus text-cumulus border-b-1 pb-[48px]">
+					Here's how we work
+				</div>
+				<div
+					class="type-headline border-b-cumulus text-cumulus border-b-1 pt-[16px] pb-[48px] font-medium">
+					We buy land in great neighborhoods near centers of
+					prosperity.
+				</div>
+				<div
+					class="type-headline border-b-cumulus text-cumulus border-b-1 pt-[16px] pb-[48px] font-medium">
+					We manufacture every component in automated U.S. factories.
+				</div>
+				<div
+					class="type-headline border-b-cumulus text-cumulus border-b-1 pt-[16px] pb-[48px] font-medium">
+					We design and engineer attractive, modular homes optimized
+					for families.
+				</div>
+				<div
+					class="type-headline border-b-cumulus text-cumulus border-b-1 pt-[16px] pb-[48px] font-medium">
+					We deliver the building parts across the nation and assemble
+					on-site.
+				</div>
+				<div
+					class="type-headline border-b-cumulus text-cumulus border-b-1 pt-[16px] pb-[48px] font-medium">
+					We sell and rent these homes directly to people.
+				</div>
+			</section>
+			<div class="h-[48px] md:h-[120px]"></div>
+			<div class="flex flex-col md:flex-row">
 				<div
 					class="bg-driveway/20 mr-6 aspect-[560/644] h-auto w-full flex-1/2 rounded-[2px] lg:aspect-[684/855]">
 				</div>
+				<div class="h-[24px] md:hidden"></div>
 				<div
 					class="md:border-l-cumulus flex flex-1/2 flex-col md:border-l-1 md:pl-6">
 					<div
-						class="text-cumulus type-display font-medium text-balance">
+						class="text-cumulus type-body md:type-display font-medium text-balance">
 						<p>
 							Block by block, city by city, let’s build a new home
 							for the American Dream.
